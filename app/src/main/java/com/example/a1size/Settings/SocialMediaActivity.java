47137f934610501035120b60cb1e342cca779ea4
Eric <eric.reeves@faulkner.edu>
2019-05-01 12:59:13 -0500

    Added buttons to settings

---
+++ app/src/main/java/com/example/a1size/Settings/SocialMediaActivity.java
@@ -0,0 +1,15 @@
+package com.example.a1size.Settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a1size.R;
+
+public class SocialMediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_media);
+    }
+}
---
+++ app/src/main/java/com/example/a1size/Settings/TermServicesActivity.java
@@ -0,0 +1,15 @@
+package com.example.a1size.Settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a1size.R;
+
+public class TermServicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_services);
+    }
+}
